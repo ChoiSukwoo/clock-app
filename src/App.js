97f44908421663a6df5fd9c2a8db3e3aa9e651f1
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2021-11-29 23:35:17 +0900

    API로 데이터 가져오기
    API로 데이터 가져오기
    graphql실패

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useState,useEffect } from 'react';
+import axios from "axios"
+
 import './App.css';
 
-function App() {
-  return (
-    <div>
 
-    </div>
+
+function App() {
+
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getIp = async() =>{
+    const { data } = await axios.get(
+      `https://api.freegeoip.app/json/?apikey=8d6814b0-4ae3-11ec-ac38-13d6984d8b0f`
+    )
+    console.log("위치정보 : ", data)
+    getTime(data['ip'])
+  }
+
+  const getTime = async(ip) =>{
+    const { data } = await axios.get(
+      `http://worldtimeapi.org/api/ip=${ip}`
+    )
+    console.log("시간정보 : ", data)
+
+    //로딩 종료 알림
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    console.log("랜더링",isLoading)
+    getIp()
+  },[isLoading])
+
+
+
+  return (
+    isLoading ?  <div> Loading </div> : <div> end Loading </div>
   );
 }
 
